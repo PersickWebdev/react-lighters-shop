66f1c2b9535250ebc1168cd367e4903c54d7f4d6
Vladdev <vladdev@gmail.com>
2020-11-09 14:48:58 +0200

    Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import { Header } from './components';
 
 const App = () => {
   return (
-      <div className="App">
-          Main
+      <div className="application">
+          <Header/>
       </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import logo from '../../assets/images/logo.png';
+import cartIcon from '../../assets/images/cart.png';
+
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header__logoBox'>
+                <img className='header__logo' src={logo} alt=""/>
+            </div>
+            <h1 className='header__name'>
+                Exclusive Lighters
+            </h1>
+            <div className='header__rightBox'>
+                <button className='button'>
+                    Dark Theme
+                </button>
+                <div className='cart'>
+                    <NavLink className='cart__link button' to='/cart'>
+                        <div className='cart__iconBox'>
+                            <img className='cart__icon' src={cartIcon} alt="cartIcon"/>
+                        </div>
+                        <div className='cart__data'>
+                            <p className='cart__quantity'>5</p>
+                            <p className='cart__amount'>500$</p>
+                        </div>
+                    </NavLink>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
